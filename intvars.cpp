f35574a6dbe0e5497624cdf6c8411788b0ca60dc
WePlayFleX <alex.kamai@ya.ru>
2022-01-03 23:07:45 +0300

    Добавьте файлы проекта.

---
+++ intvars.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int var1;						
+	int var2;						
+	var1 = 20;						
+	var2 = var1 + 10;				
+	cout << "var1 + 10 равно ";			
+	cout << var2 << endl;			
+
+	system("pause");
+	return 0;
+}
